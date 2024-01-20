774ac8c579bff17a70015950019131b272eb1180
cristiannd <cristian.donalicio@gmail.com>
2024-01-20 11:41:35 -0300

    Add Twits card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { AuthButtonServer } from "./components/auth-button-server";
 import { redirect } from "next/navigation";
+import PostCard from "./components/post-card";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
@@ -13,15 +14,23 @@ export default async function Home() {
     redirect("/login");
   }
 
-  const { data: posts } = await supabase
-    .from("posts")
-    .select("*, users(*)");
+  const { data: posts } = await supabase.from("posts").select("*, user:users(*)");
 
-    console.log(posts)
+  console.log(posts);
 
   return (
     <main>
       <AuthButtonServer />
+
+      {posts?.map((post) => {
+        return <PostCard
+          key={post.id}
+          userFullName={post.user.raw_user_meta_data.full_name}
+          userName={post.user.raw_user_meta_data.user_name}
+          avatarUrl={post.user.raw_user_meta_data.avatar_url}
+          content={post.content}
+        />;
+      })}
     </main>
   );
 }
