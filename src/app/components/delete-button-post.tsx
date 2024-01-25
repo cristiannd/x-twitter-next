c409d59c40f96bdd84de5a45b516048cd30c87d8
cristiannd <cristian.donalicio@gmail.com>
2024-01-25 12:57:43 -0300

    Add delete icon in post card

--- src/app/components/delete-button-post.tsx
+++ src/app/components/delete-button-post.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { IconTrash } from "@tabler/icons-react";
 import { deletePost } from "../actions/post.actions";
 
 export function DeleteButtonPost({ postId }: { postId: string }) {
   return (
     <form action={() => deletePost(postId)}>
-      <button type="submit">Delete</button>
+      <button
+        type="submit"
+        className="text-gray-400 hover:text-white cursor-pointer"
+      >
+        <IconTrash />
+      </button>
     </form>
   );
 }
