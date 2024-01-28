dded6b8398695e5b1934777a4c39cf1d3b577ea7
cristiannd <cristian.donalicio@gmail.com>
2024-01-28 12:36:01 -0300

    Add styles on nav buttons

--- src/app/components/nav-links.tsx
+++ src/app/components/nav-links.tsx
@@ -10,15 +10,31 @@ export default function NavLinks() {
     {
       name: "Home",
       href: "/",
+      isSelect: false,
+    },
+    {
+      name: "Perfil",
+      href: "/profile",
+      isSelect: false,
     },
   ];
 
-  const linksToShow = links.filter((link) => link.href !== pathname);
+  const linksToShow = links.map((link) =>
+    link.href === pathname ? { ...link, isSelect: true } : link,
+  );
+
+  console.log(linksToShow);
+
+  const selectedStyle = "bg-sky-700 py-3";
 
   return (
-    <nav>
+    <nav className="flex gap-1">
       {linksToShow.map((link) => (
-        <Link key={link.href} href={link.href}>
+        <Link
+          key={link.href}
+          href={link.href}
+          className={`h-fit w-auto rounded-b p-2 font-bold hover:bg-sky-500 ${link.isSelect ? "bg-sky-500 pt-3" : "bg-gray-300 text-gray-800"}`}
+        >
           {link.name}
         </Link>
       ))}
