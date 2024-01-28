09605c2b2d18f2cd725fdde02cd27973f11ec63e
cristiannd <cristian.donalicio@gmail.com>
2024-01-27 21:58:03 -0300

    Change button style

--- src/app/components/delete-button-post.tsx
+++ src/app/components/delete-button-post.tsx
@@ -19,26 +19,24 @@ export function DeleteButtonPost({ postId }: { postId: string }) {
   };
 
   return (
-    <>
-      <Popover
-        placement="right"
-        isOpen={isOpen}
-        onOpenChange={(open) => setIsOpen(open)}
-      >
-        <PopoverTrigger>
-          <Button className="text-gray-400 hover:text-white cursor-pointer bg-transparent">
-            <IconTrash />
-          </Button>
-        </PopoverTrigger>
-        <form action={() => deleteHandler(postId)}>
-          <PopoverContent className="bg-red-500">
-            <button type="submit" className="px-1 py-1">
-              <div className="text-small font-bold">Delete post</div>
-              <div className="text-tiny">This action is permanent</div>
-            </button>
-          </PopoverContent>
-        </form>
-      </Popover>
-    </>
+    <Popover
+      placement="right"
+      isOpen={isOpen}
+      onOpenChange={(open) => setIsOpen(open)}
+    >
+      <PopoverTrigger className="p-0 m-0">
+        <Button className="min-w-fit cursor-pointer bg-transparent text-gray-400 hover:text-white">
+          <IconTrash />
+        </Button>
+      </PopoverTrigger>
+      <form action={() => deleteHandler(postId)}>
+        <PopoverContent className="bg-red-500">
+          <button type="submit" className="px-1 py-1">
+            <div className="text-small font-bold">Delete post</div>
+            <div className="text-tiny">This action is permanent</div>
+          </button>
+        </PopoverContent>
+      </form>
+    </Popover>
   );
 }
